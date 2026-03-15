06a37c2bad8bb95f150b74ce4e025ee67a6a4a89
Ali Imam <syedaliimamshah04@gmail.com>
2026-03-15 16:54:44 +0500

    Organized all files into one folder

---
+++ if_else(conditions)/problem_1.py
@@ -0,0 +1,12 @@
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+d=int(input("enter a number:"))
+if(a>b and a>c and a>d):
+    print("a is the largest number",a)
+if(b>a and b>c and b>d):
+    print("b is the largest number",b)
+if(c>b and c>a and c>d):
+    print("a is the largest number",c)
+if(d>b and d>c and d>a):
+    print("a is the largest number",d)
---
+++ if_else(conditions)/problem_5.py
@@ -0,0 +1,6 @@
+l=["harry","sachin","rahul","rohit","virat"]
+a=input("enter the name you want to search:")
+if(a in l):
+    print("the name is present in the list")
+else:
+    print ("the name is not present in the list")
