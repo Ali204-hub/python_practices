c3fbbb609ec4509c14c369c2275d61d18fe51290
Ali Imam <syedaliimamshah04@gmail.com>
2026-03-27 00:22:09 +0500

    Removed list_question folder

--- list_question/check whether a list is sorted or not.py
+++
@@ -1,9 +0,0 @@
-l=[1,2,3,6,4,20]
-for i in range(len(l)-1):
-    if l[i]<l[i+1]:
-        continue
-    else:
-        print("your list is not sorted")
-        break
-else:
-    print("your list is sorted")
--- list_question/mean_find.py
+++
@@ -1,5 +0,0 @@
-l=[13,23,45,-44,55]
-sum=0
-for i in l:
-    sum+=i
-print(sum/len(l))
